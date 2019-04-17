6801099d920690386964946ab8082b79ce9bda57
baremetalfreak <vit.gottwald@gmail.com>
2019-04-17 18:40:37 +0200

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# react-hooks-async-callback
-Created with CodeSandbox
---
+++ src/useAsyncCallback.js
@@ -0,0 +1,19 @@
+import { useRef, useEffect, useCallback } from "react";
+
+export default function(callback) {
+  const mounted = useRef(false);
+  useEffect(() => {
+    mounted.current = true;
+    return () => {
+      mounted.current = false;
+    };
+  }, []);
+  const whileMounted = (...args) => {
+    if (mounted.current) {
+      return callback.apply(null, args);
+    } else {
+      console.log("Skipping because not mounted");
+    }
+  };
+  return useCallback(whileMounted, [callback]);
+}
